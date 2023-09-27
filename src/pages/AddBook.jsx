982a967b5a36fc80b866f380e5e2b74c55d3981c
Tanjil Hossain <tanjilhossain845.com>
2023-09-27 21:26:49 +0600

    compleate add vidoo funtionality

--- src/pages/AddBook.jsx
+++ src/pages/AddBook.jsx
@@ -1,10 +1,30 @@
+import { useEffect, useState } from "react";
+import { useAddBookMutation } from "../redux/features/api/apiSlice";
+import { useNavigate } from "react-router-dom";
+
 const AddBook = () => {
+  const [addBook, { isLoading, isSuccess }] = useAddBookMutation();
+  const [name, setName] = useState("");
+  const [author, setAuthor] = useState("");
+  const [thumbnail, setThumnail] = useState("");
+  const [price, setPrice] = useState("");
+  const [rating, setRating] = useState("");
+  const [featured, setFeatured] = useState(false);
+  const navigate = useNavigate();
+
+  const handelAddBook = (e) => {
+    e.preventDefault();
+    addBook({ name, author, thumbnail, price, rating, featured });
+  };
+  useEffect(() => {
+    isSuccess && navigate("/");
+  }, [isSuccess, navigate]);
   return (
     <main className="py-6 2xl:px-6">
       <div className="container">
         <div className="p-8 overflow-hidden bg-white shadow-cardShadow rounded-md max-w-xl mx-auto">
           <h4 className="mb-8 text-xl font-bold text-center">Add New Book</h4>
-          <form className="book-form">
+          <form className="book-form" onSubmit={handelAddBook}>
             <div className="space-y-2">
               <label>Book Name</label>
               <input
@@ -13,6 +33,8 @@ const AddBook = () => {
                 type="text"
                 id="lws-bookName"
                 name="name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
               />
             </div>
 
@@ -24,6 +46,8 @@ const AddBook = () => {
                 type="text"
                 id="lws-author"
                 name="author"
+                value={author}
+                onChange={(e) => setAuthor(e.target.value)}
               />
             </div>
 
@@ -35,6 +59,8 @@ const AddBook = () => {
                 type="text"
                 id="lws-thumbnail"
                 name="thumbnail"
+                value={thumbnail}
+                onChange={(e) => setThumnail(e.target.value)}
               />
             </div>
 
@@ -47,6 +73,8 @@ const AddBook = () => {
                   type="number"
                   id="lws-price"
                   name="price"
+                  value={price}
+                  onChange={(e) => setPrice(e.target.value)}
                 />
               </div>
 
@@ -60,6 +88,8 @@ const AddBook = () => {
                   name="rating"
                   min="1"
                   max="5"
+                  value={rating}
+                  onChange={(e) => setRating(e.target.value)}
                 />
               </div>
             </div>
@@ -70,11 +100,18 @@ const AddBook = () => {
                 type="checkbox"
                 name="featured"
                 className="w-4 h-4"
+                value={featured}
+                onChange={(e) => setFeatured(e.target.checked)}
               />
               <label className="ml-2 text-sm"> This is a featured book </label>
             </div>
 
-            <button type="submit" className="submit" id="lws-submit">
+            <button
+              type="submit"
+              className="submit"
+              id="lws-submit"
+              disabled={isLoading}
+            >
               Add Book
             </button>
           </form>
--- src/redux/features/api/apiSlice.jsx
+++ src/redux/features/api/apiSlice.jsx
@@ -3,14 +3,25 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:9000" }),
+  tagTypes: ["Books"],
   endpoints: (builder) => ({
     getBooks: builder.query({
       query: () => ({
         url: "/books",
         method: "GET",
       }),
+      keepUnusedDataFor: 300,
+      providesTags: ["Books"],
+    }),
+    addBook: builder.mutation({
+      query: (data) => ({
+        url: "/books",
+        method: "POST",
+        body: data,
+      }),
+      invalidatesTags:['Books']
     }),
   }),
 });
 
-export const { useGetBooksQuery } = apiSlice;
+export const { useGetBooksQuery, useAddBookMutation } = apiSlice;
